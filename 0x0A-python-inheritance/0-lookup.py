93d24365a078fda9d3f5069a82231e28ffa4639f
lucky-blessed <blessednkwor7@gmail.com>
2024-01-10 20:38:04 +0100

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-"""
-Module for Task 0.
-"""
+"""Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    """
-    Returns a list of available atrributes and methods of an object.
-    Args:
-        obj: The object to inspect.
-
-    Returns:
-        A list of strings representing the attributes and methods of the object.
-    """
-    return dir(obj)
+    """Return a list of an object's available attributes."""
+    return (dir(obj))

ece7c67bf05b6efbbd3dedb98e8347c4185c03c2
lucky-blessed <blessednkwor7@gmail.com>
2024-01-10 21:55:39 +0100

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""
+Task 0 Module.
+"""
+
+
+def read_file(filename=""):
+    """
+    Prints the content of utf8 file to stdout
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")

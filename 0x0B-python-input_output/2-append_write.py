8d1c6a5706553c7cca3bd21f99e8d1d085789e0e
lucky-blessed <blessednkwor7@gmail.com>
2024-01-11 03:12:08 +0100

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module documentation goes here
+"""
+
+def append_write(filename="", text=""):
+    """
+    Function documentation goes here
+
+    Args:
+        filename (str): The name of the file to which text will be appended.
+        text (str): The text to be appended to the file.
+
+    Returns:
+        int: The number of characters added.
+
+    """
+    with open(filename, mode='a', encoding='utf-8') as file:
+        return file.write(text)

f53af50c8e9d08f155ff3a5994a003bedcc92374
lucky-blessed <blessednkwor7@gmail.com>
2024-02-15 21:55:42 +0100

    6-list_values.sql modified

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
--- Prints the full description of the table first_table
-
 -- Print the full description using INFORMATION_SHEMA
 SHOW CREATE TABLE 'first_table';
--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,4 +1,2 @@
--- List all rows of the table first_table
-
 -- Displaying all rows in the specific database and table
-SELCT * FROM hbtn_0c_0.first_table;
+SELCT * FROM first_table;

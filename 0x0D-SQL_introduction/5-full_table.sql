77918c462d31ecdd220da6ac13232bfc08c83f30
lucky-blessed <blessednkwor7@gmail.com>
2024-02-15 21:52:36 +0100

    5-full_table.sql refactored

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
 -- Prints the full description of the table first_table
 
 -- Print the full description using INFORMATION_SHEMA
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABKE_NAME = 'first_table';
+SHOW CREATE TABLE 'first_table';

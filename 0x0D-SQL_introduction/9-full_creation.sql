7f19911f66ddc4bd55b51e29949725043c43c742
lucky-blessed <blessednkwor7@gmail.com>
2024-02-16 00:41:26 +0100

    9-full_creation.sql refactored

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,12 +1,16 @@
 -- Create the table second_table and add multiple rows
 
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT
+CREATE TABLE IF NOT EXISTS `second_table` (
+	`id` INT,
+	`name` VARCHAR(256),
+	`score` INT
 );
-INSERT INTO hbtn_0c_0.second_table (`id`, `name`, `score`) VALUES
-	(1, "John", 10)
-	(2, "Alex", 3)
-	(3, "Bob", 14)
-	(4, "George", 8);
+INSERT INTO `second_table` (
+	`id`,
+	`name`,
+	`score`)
+VALUES
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);

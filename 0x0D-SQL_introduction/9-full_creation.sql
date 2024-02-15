9a491aed9b23c827d37eba09289358a0613cc07d
lucky-blessed <blessednkwor7@gmail.com>
2024-02-15 23:55:35 +0100

    9-full_creation.sql modified

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,12 +1,12 @@
--- Create the table second_table and add multiple rows.\
+-- Create the table second_table and add multiple rows
 
 CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
-	);
+);
 INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
-(1, 'John', 10)
-(2, 'Alex', 3)
-(3, 'Bob', 14)
-(4, 'George', 8);
+	(1, "John", 10)
+	(2, "Alex", 3)
+	(3, "Bob", 14)
+	(4, "George", 8);

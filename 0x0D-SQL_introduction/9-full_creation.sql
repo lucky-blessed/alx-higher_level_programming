610971831f6397ae159a1aa2f8e3df441db6ad19
lucky-blessed <blessednkwor7@gmail.com>
2024-02-16 00:29:34 +0100

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
 	name VARCHAR(256),
 	score INT
 );
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
+INSERT INTO hbtn_0c_0.second_table (`id`, `name`, `score`) VALUES
 	(1, "John", 10)
 	(2, "Alex", 3)
 	(3, "Bob", 14)

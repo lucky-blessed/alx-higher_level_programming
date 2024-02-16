0adde774fa9c624d3681ef080ac2aa7c749ca5ed
lucky-blessed <blessednkwor7@gmail.com>
2024-02-16 01:54:30 +0100

    100-move_to_utf8.sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- Convert hbtn_0c_0 database, first_table and name field to UTF8
+
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb884_unicode_ci;
+
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utfbmb4_unicode_ci;
